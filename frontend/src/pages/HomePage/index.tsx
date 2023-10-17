77c798eaa6f01b8698b6cba21f3a019e2bf7235e
Luca Villela <lucatvillela@gmail.com>
2023-10-17 19:55:49 -0300

    Added search input with google autocomplete

--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -1,32 +1,88 @@
-import React from 'react';
-import { Box } from '@mui/material';
-import { useJsApiLoader, GoogleMap } from '@react-google-maps/api';
+import { useState, useEffect } from "react";
+import HiddenComponent from "../../components/HiddenComponent";
+import { Box, TextField, styled } from "@mui/material";
+import { useJsApiLoader, GoogleMap, Autocomplete, LoadScript } from "@react-google-maps/api";
+
 
 const HomePage = () => {
-  const apiKey = 'AIzaSyD3eD__3scBZrYIqi9DMB0YQy7WaP7jcSw'; 
+  const apiKey = "AIzaSyD3eD__3scBZrYIqi9DMB0YQy7WaP7jcSw";
+
+  const [latitudeUser, setLatitudeUser] = useState(-12.97);
+  const [longitudeUser, setLongitudeUser] = useState(-38.512);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitudeUser(position.coords.latitude);
+      setLongitudeUser(position.coords.longitude);
+    });
+  }, []);
+
+  const mapSettings = {
+    mapId: "b8c651ba52d26afb",
+    clickableIcons: false,
+    streetViewControl: false,
+    mapTypeControl: false,
+    fullscreenControl: false,
+    center: {
+      lat: latitudeUser,
+      lng: longitudeUser,
+    },
+    zoom: 16,
+  };
 
   const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
     googleMapsApiKey: apiKey,
+    libraries: ['places'],
   });
 
-  if (!isLoaded) {
-    return <div>Erro!</div>;
-  }
+  const SearchContainer = styled(Box)({
+    padding: "20px",
+    position: "absolute",
+    zIndex: "10",
+    boxShadow: "0 0 10px 0px rgba(0, 0, 0, 0.2)",
+    marginTop: "50px",
+    marginLeft: "50px",
+    backgroundColor: "#FFFFFF",
+    display: "flex",
+    width: "auto",
+    borderRadius: "10px"  
+
+  });
+
+  const LocationInput = styled(TextField)({
+    width: "400px",
+  });
+
+  const MapBox = styled(Box)({
+    display: "flex",
+    justifyContent: "left",
+  });
 
   return (
-    <Box position='absolute' left={0} top={0} height='100%' width='100%'>
-      {isLoaded ? (
-        <GoogleMap
-          mapContainerStyle={{ width: '100%', height: '100%' }}
-          center={{ lat: -34.397, lng: 150.644 }}
-          zoom={8}
-        >
-        </GoogleMap>
-      ) : (
-        <div>Carregando Google Maps...</div>
-      )}
-    </Box>
+    <div>
+      <MapBox left={0} top={0} height="90vh" width="100%">
+        <LoadScript id="script-loader" googleMapsApiKey="AIzaSyD3eD__3scBZrYIqi9DMB0YQy7WaP7jcSw" libraries={["places"]}>
+          <SearchContainer>
+            <Autocomplete>
+              <LocationInput id="outlined-basic" label="Procurar corretores em..." variant="outlined"  />
+            </Autocomplete>
+          </SearchContainer>
+
+          <HiddenComponent hidden={!isLoaded}>
+            <GoogleMap
+              mapContainerStyle={{ width: "100%", height: "100%" }}
+              center={mapSettings.center}
+              zoom={mapSettings.zoom}
+              options={mapSettings}
+              ></GoogleMap>
+          </HiddenComponent>
+
+          <HiddenComponent hidden={isLoaded}>
+            <div>Carregando Google Maps...</div>
+          </HiddenComponent>
+        </LoadScript>
+      </MapBox>
+    </div>
   );
 };
 
