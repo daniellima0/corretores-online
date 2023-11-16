2d5078930c9d9e452dc4ee6ca8222e473413dbd0
Daniel Lima <danielmlimag@gmail.com>
2023-11-16 12:58:21 -0300

    feat: create settings page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import RealtorProfile from "./pages/RealtorProfile";
+import Settings from "./pages/Settings";
 
 const router = createBrowserRouter([
-    {
-        path: "/landing-page",
-        element: <LandingPage />,
-    },
-    {
-        path: "/realtor-profile",
-        element: <RealtorProfile />,
-    },
+  {
+    path: "/landing-page",
+    element: <LandingPage />,
+  },
+  {
+    path: "/realtor-profile",
+    element: <RealtorProfile />,
+  },
+  {
+    path: "/settings",
+    element: <Settings />,
+  },
 ]);
 
 function App() {
-    return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- frontend/src/components/RoundedButton.tsx
+++ frontend/src/components/RoundedButton.tsx
@@ -1,23 +1,48 @@
-import { styled } from "@mui/material/styles";
+import styled from "@emotion/styled";
 import Button from "@mui/material/Button";
 
-const Container = styled(Button)`
-    width: fit-content;
-    align-self: center;
-    border-radius: 20px;
-    background-color: ${({ theme }) => theme.customPallete.costumer};
-`;
+const Container = styled(Button, {
+  // This shouldForwardProp is needed to prevent emotion from passing the buttonColor and invertColor props to the DOM element as attributes
+  shouldForwardProp: (prop) => prop !== "buttonColor" && prop !== "invertColor",
+})<{
+  buttonColor: string;
+  invertColor: boolean;
+}>(
+  {
+    width: "fit-content",
+    alignSelf: "center",
+    borderRadius: "20px",
+  },
+  (props) => ({
+    backgroundColor: props.invertColor ? "white" : props.buttonColor,
+    border: `1px solid ${props.buttonColor}`,
+    color: props.invertColor ? props.buttonColor : "white",
+    ":hover": {
+      backgroundColor: props.invertColor ? "white" : props.buttonColor,
+      border: `1px solid ${props.buttonColor}`,
+      color: props.invertColor ? props.buttonColor : "white",
+    },
+  })
+);
 
 interface RoundedButtonProps {
-    onClick?: () => void;
+  onClick?: () => void;
+  children?: string;
+  buttonColor?: string;
+  invertColor?: boolean;
 }
 
 const RoundedButton: React.FC<RoundedButtonProps> = (props) => {
-    return (
-        <Container onClick={props.onClick} variant="contained">
-            Quero entrar em contato
-        </Container>
-    );
+  return (
+    <Container
+      buttonColor={props.buttonColor || "#000"}
+      invertColor={props.invertColor || false}
+      onClick={props.onClick}
+      variant="contained"
+    >
+      {props.children}
+    </Container>
+  );
 };
 
 export default RoundedButton;
